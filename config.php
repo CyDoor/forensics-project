fdcecb0227e5bd6ef451935868cf3fdde877d444
Robert Ozimek <robertozimek90@gmail.com>
2015-12-03 01:44:05 -0500

    Added login script found online

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "secure_user");    // The database username. 
+define("PASSWORD", "t3st3rl0g1n");    // The database password. 
+define("DATABASE", "loginscript");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
